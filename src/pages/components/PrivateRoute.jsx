a1087fa272a1ede2a488640334db5143d6e3d826
vivcis <ceciliaorji.co@gmail.com>
2023-04-22 19:20:49 +0100

    Create a spinner component

--- src/pages/components/PrivateRoute.jsx
+++ src/pages/components/PrivateRoute.jsx
@@ -1,10 +1,11 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import useAuthStatus from '../../hooks/useAuthStatus';
+import Spinner from './Spinner';
 
 export default function PrivateRoute() {
     const {loggedIn, checkingStatus } = useAuthStatus();
     if(checkingStatus){
-        return <h3>loading...</h3>
+        return <Spinner />;
     }
 
   return loggedIn ? <Outlet /> : <Navigate to= "/sign-in" />;
