eda25a252dfffd93cdfa664d97ac1e65d7eeeb2e
vivcis <ceciliaorji.co@gmail.com>
2023-05-31 13:49:00 +0100

    fix the no current offers section

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -89,7 +89,9 @@ export default function Category() {
           )}
         </>
       ) : (
-        <p>There are no current offers</p>
+        <p>There are no current 
+            {params.categoryName === "rent" ? "places for rent" : "places for sale"}
+            </p>
       )}
     </div>
   )
